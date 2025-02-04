d0b37f850cd3310b30e8ef9c06e4a8d4520678af
keypax <pop_gniezno@interia.pl>
2025-02-04 15:46:24 +0100

    cc

--- src/Controller/Admin/AnimalController.php
+++ src/Controller/Admin/AnimalController.php
@@ -13,10 +13,21 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/admin/animal', name: 'admin_animal_')]
 class AnimalController extends AbstractController
 {
-    #[Route('/add', name: 'add')]
-    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/edit/{animal_id}', name: 'edit', defaults: ['animal_id' => null])]
+    public function manageAnimal(Request $request, EntityManagerInterface $entityManager, string $animal_id = null): Response
     {
-        $animal = new Animal();
+        if ($animal_id) {
+            $animal = $entityManager->getRepository(Animal::class)->findOneBy(["animal_id" => $animal_id]);
+
+            if (!$animal) {
+                throw $this->createNotFoundException('Animal not found');
+            }
+
+            $flashMessage = 'Animal updated successfully!';
+        } else {
+            $animal = new Animal();
+            $flashMessage = 'Animal created successfully!';
+        }
 
         $form = $this->createForm(AnimalType::class, $animal);
 
@@ -25,39 +36,14 @@ class AnimalController extends AbstractController
             $entityManager->persist($animal);
             $entityManager->flush();
 
-            $this->addFlash('success', 'Animal created successfully!');
-
-            return $this->redirectToRoute('admin_animal_index');
-        }
-
-        return $this->render('admin/animal/add.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
-    #[Route('/edit/{animal_id}', name: 'edit')]
-    public function edit(Request $request, EntityManagerInterface $entityManager, string $animal_id): Response
-    {
-        $animal = $entityManager->getRepository(Animal::class)->findBy("animal_id", $animal_id);
-
-        if (!$animal) {
-            throw $this->createNotFoundException('Animal not found');
-        }
-
-        $form = $this->createForm(AnimalType::class, $animal);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush(); // W przypadku edycji nie używamy persist(), bo obiekt jest już zarządzany przez Doctrine
-
-            $this->addFlash('success', 'Animal updated successfully!');
+            $this->addFlash('success', $flashMessage);
 
             return $this->redirectToRoute('admin_animal_index');
         }
 
         return $this->render('admin/animal/edit.html.twig', [
             'form' => $form->createView(),
-            'animal' => $animal, // Możesz przekazać zwierzę do szablonu, jeśli potrzebujesz wyświetlić np. tytuł "Edycja zwierzęcia: ..."
+            'animal' => $animal ?? null,
         ]);
     }
 
