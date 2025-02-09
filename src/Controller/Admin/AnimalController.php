f176e7a12c0ac37f5071a2b4cc2ad0b5eca45cea
keypax <pop_gniezno@interia.pl>
2025-02-09 22:06:24 +0100

    cc

--- src/Controller/Admin/AnimalController.php
+++ src/Controller/Admin/AnimalController.php
@@ -3,19 +3,23 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Animal;
+use App\Entity\AnimalPhoto;
 use App\Form\AnimalPhotoType;
 use App\Form\AnimalType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/animal', name: 'admin_animal_')]
 class AnimalController extends AbstractController
 {
     #[Route('/edit/{animal_id}', name: 'edit', defaults: ['animal_id' => null])]
-    public function manageAnimal(Request $request, EntityManagerInterface $entityManager, string $animal_id = null): Response
+    public function manageAnimal(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger, string $animal_id = null): Response
     {
         $editMode = false;
         if ($animal_id) {
@@ -36,6 +40,7 @@ class AnimalController extends AbstractController
         $formPhoto = $this->createForm(AnimalPhotoType::class);
 
         $form->handleRequest($request);
+        $formPhoto->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             //change "/" to "-"
             $currentAnimalId = $animal->getAnimalId();
@@ -46,9 +51,48 @@ class AnimalController extends AbstractController
             }
 
             $photo = $formPhoto->get('photo')->getData();
-            if ($photo)
-            {
-                echo "There is photo";
+            if ($photo) {
+                $year = date('Y');
+                $month = date('m');
+
+                $projectDir = $this->getParameter('kernel.project_dir');
+                $localDir = '/photos/animals/' . $year . '/' . $month . '/' . $animal->getAnimalId();
+                $targetDirectory = $projectDir . '/public' . $localDir;
+
+                $filesystem = new Filesystem();
+                try {
+                    $filesystem->mkdir($targetDirectory, 0777);
+                } catch (\Exception $e) {
+                    $this->addFlash('error', 'Nie udało się utworzyć katalogu dla zdjęcia.');
+                    return $this->redirectToRoute('admin_animal_index');
+                }
+
+                $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$photo->guessExtension();
+
+                try {
+                    $photo->move($targetDirectory, $newFilename);
+
+                    $imageSize = getimagesize($targetDirectory . '/' . $newFilename);
+                    $width = $imageSize[0];
+                    $height = $imageSize[1];
+                    $fileSize = filesize($targetDirectory . '/' . $newFilename);
+
+                    $animalPhoto = new AnimalPhoto();
+
+                    $animalPhoto->setFilename($localDir . '/' . $newFilename);
+                    $animalPhoto->setWidth($width);
+                    $animalPhoto->setHeight($height);
+                    $animalPhoto->setSize($fileSize);
+
+                    $animalPhoto->setAnimal($animal);
+
+                    $entityManager->persist($animalPhoto);
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'Nie udało się przenieść zdjęcia.');
+                    return $this->redirectToRoute('admin_animal_index');
+                }
             }
 
             $entityManager->persist($animal);
