ca590d5fa7825680b8e3aa112841f2114c818dbf
keypax <pop_gniezno@interia.pl>
2025-02-10 23:33:20 +0100

    cc

--- src/Service/AnimalPhotoService.php
+++ src/Service/AnimalPhotoService.php
@@ -4,12 +4,13 @@ namespace App\Service;
 
 use App\Entity\Animal;
 use App\Entity\AnimalPhoto;
+use App\Service\FileUploader\FileUploaderInterface;
+use App\Service\ThumbnailGenerator\ThumbnailGeneratorInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Imagine\Gd\Imagine;
 use Imagine\Image\Box;
 use Imagine\Image\ImageInterface;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\String\AbstractUnicodeString;
@@ -19,18 +20,17 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 class AnimalPhotoService
 {
     public function __construct(
-        private Filesystem $filesystem,
+        private FileUploaderInterface $fileUploader,
+        private ThumbnailGeneratorInterface $thumbnailGenerator,
         private SluggerInterface $slugger,
         private EntityManagerInterface $entityManager,
         private TranslatorInterface $translator,
         private LoggerInterface $logger,
         private string $basePathServer,
         private string $basePathWeb,
-        private int $thumbnailMaxSizeMedium,
-        private int $thumbnailMaxSizeSmall,
+        private array $thumbnailSizes,
         private bool $changeExtension,
-        private string $targetExtension,
-        private Imagine $imagine = new Imagine()
+        private string $targetExtension
     ) { }
 
     /**
@@ -44,12 +44,14 @@ class AnimalPhotoService
         $targetServerDirectory = $this->basePathServer . $year . '/' . $month . '/' . $animal->getAnimalId();
         $targetWebDirectory = $this->basePathWeb . $year . '/' . $month . '/' . $animal->getAnimalId();
 
-        $this->createDirectory($targetServerDirectory);
+        $this->fileUploader->createDirectory($targetServerDirectory);
 
         $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
         $safeFilename = $this->slugger->slug($originalFilename);
         $newFilename = $this->getNewFilename($safeFilename, $photo);
         $originalPath = $targetServerDirectory . '/' . $newFilename;
+
+        $originalServerPath = $this->fileUploader->upload($photo, $targetServerDirectory, $newFilename);
 
         try {
             $photo->move($targetServerDirectory, $newFilename);
@@ -66,7 +68,12 @@ class AnimalPhotoService
             $animalPhoto->setHeight($height);
             $animalPhoto->setSize($fileSize);
 
-            $this->generateThumbnails($originalPath, $targetServerDirectory, $newFilename);
+            $this->thumbnailGenerator->generateThumbnails( // Use ThumbnailGenerator service
+                $originalServerPath,
+                $targetServerDirectory,
+                pathinfo($newFilename, PATHINFO_FILENAME), // Pass filename without extension for thumbnails
+                $this->thumbnailSizes // Pass thumbnail sizes configuration
+            );
 
             $this->entityManager->persist($animalPhoto);
             $this->entityManager->flush();
@@ -80,51 +87,6 @@ class AnimalPhotoService
         }
     }
 
-    private function generateThumbnails(string $originalPath, string $targetDirectory, string $filename): void
-    {
-        $originalImage = $this->imagine->open($originalPath);
-        $originalWidth = $originalImage->getSize()->getWidth();
-        $originalHeight = $originalImage->getSize()->getHeight();
-
-        $mediumFilename = pathinfo($filename, PATHINFO_FILENAME) . '_medium.jpg';
-        $mediumTargetFilename = $targetDirectory . '/' . $mediumFilename;
-        $mediumMaxSize = $this->thumbnailMaxSizeMedium;
-
-        $this->resize($originalWidth, $originalHeight, $mediumMaxSize, $originalImage, $mediumTargetFilename);
-
-        $thumbnailFilename = pathinfo($filename, PATHINFO_FILENAME) . '_small.jpg';
-        $thumbnailTargetFilename = $targetDirectory . '/' . $thumbnailFilename;
-        $thumbnailMaxSize = $this->thumbnailMaxSizeSmall;
-
-        $this->resize($originalWidth, $originalHeight, $thumbnailMaxSize, $originalImage, $thumbnailTargetFilename);
-    }
-
-    private function resize(
-        int $originalWidth,
-        int $originalHeight,
-        int $mediumMaxSize,
-        ImageInterface $originalImage,
-        string $mediumTargetFilename,
-    ): void
-    {
-        if ($originalWidth > $originalHeight) {
-            $mediumWidth = $mediumMaxSize;
-            $mediumHeight = (int)($originalHeight * ($mediumMaxSize / $originalWidth));
-        } else {
-            $mediumHeight = $mediumMaxSize;
-            $mediumWidth = (int)($originalWidth * ($mediumMaxSize / $originalHeight));
-        }
-
-        $mediumSize = new Box($mediumWidth, $mediumHeight);
-        $mediumImage = $originalImage->resize($mediumSize);
-
-        if ($this->changeExtension) {
-            $mediumImage->save($mediumTargetFilename, ['format' => $this->targetExtension]);
-        }
-        else {
-            $mediumImage->save($mediumTargetFilename);
-        }
-    }
 
     private function getNewFilename(AbstractUnicodeString $safeFilename, UploadedFile $photo): string
     {
@@ -135,19 +97,5 @@ class AnimalPhotoService
         }
 
         return $newFilename;
-    }
-
-    /**
-     * @param string $targetDirectory
-     * @return void
-     * @throws \Exception
-     */
-    private function createDirectory(string $targetDirectory): void
-    {
-        try {
-            $this->filesystem->mkdir($targetDirectory, 0755);
-        } catch (\Exception $e) {
-            throw new \Exception($this->translator->trans('error_creating_directory'));
-        }
     }
 }
