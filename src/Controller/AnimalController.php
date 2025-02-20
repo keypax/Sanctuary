25f026814648d7965bb0d73ab7c2f78740741a44
keypax <pop_gniezno@interia.pl>
2025-02-20 14:35:02 +0100

    cc

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -63,88 +63,42 @@ class AnimalController extends AbstractController
         ]);
     }
 
-    #[Route('/edit/{id}', name: 'edit', defaults: ['id' => null])]
+    #[Route('/edit/{id}', name: 'edit', defaults: ['id' => null], methods: ['GET', 'POST'])]
     public function edit(
+        Request $request,
+        AnimalIdGenerationStrategyInterface $nextAnimalIdProvider,
         string $id
     ): Response {
         $animal = $this->animalRepository->getById($id);
 
         $form = $this->createForm(AnimalType::class, $animal);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $this->entityManager->persist($animal);
+                $this->entityManager->flush();
+
+                $nextAnimalIdProvider->incrementId();
+
+                $this->addFlash('success', $this->translator->trans('animal.form.success'));
+
+                return $this->redirectToRoute('animal_index');
+            }
+            catch (UniqueConstraintViolationException) {
+                $this->addFlash('error', $this->translator->trans('animal.form.duplication_error'));
+
+                $form->get('animal_id')->addError(new FormError($this->translator->trans('animal.form.duplication_error')));
+            }
+        }
 
         return $this->render('animal/edit.html.twig', [
-            'form' => $form->createView(),
+            'form' => $form,
             'animal' => $animal,
         ]);
     }
 
-    /*public function manageAnimal(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        AnimalIdGenerationStrategyInterface $nextAnimalIdProvider,
-        string $animal_id = null
-    ): Response
-    {
-        $editMode = false;
-        if ($animal_id) {
-            $animal = $entityManager->getRepository(Animal::class)->findOneBy(["animal_id" => $animal_id]);
-            $editMode = true;
-
-            if (!$animal) {
-                throw $this->createNotFoundException('Animal not found');
-            }
-        } else {
-            $animal = new Animal();
-            $animal->setAnimalId($nextAnimalIdProvider->proposeNextId());
-        }
-
-        $form = $this->createForm(AnimalType::class, $animal);
-        $formPhoto = $this->createForm(AnimalPhotoType::class);
-
-        $form->handleRequest($request);
-        $formPhoto->handleRequest($request);
-        if ($formPhoto->isSubmitted() && $formPhoto->isValid()) {
-            $photo = $formPhoto->get('photo')->getData();
-            if ($photo) {
-                $animalPhoto = $this->animalPhotoService->uploadAnimalPhoto($photo, $animal);
-
-                if (!$animalPhoto) {
-                    $this->addFlash('error', 'Nie udało się zapisać zdjęcia.');
-                    return $this->redirectToRoute('animal_index');
-                }
-            }
-        }
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $currentAnimalId = $animal->getAnimalId();
-
-            //change "/" to "-"
-            if ($currentAnimalId) {
-                $newAnimalId = str_replace('/', '-', $currentAnimalId);
-                $animal->setAnimalId($newAnimalId);
-            }
-
-            if (!$editMode)
-            {
-                $nextAnimalIdProvider->incrementId();
-            }
-
-            $entityManager->persist($animal);
-            $entityManager->flush();
-
-            //$this->addFlash('success', $flashMessage);
-
-            return $this->redirectToRoute('animal_index');
-        }
-
-        return $this->render('animal/edit.html.twig', [
-            'form' => $form->createView(),
-            'form_photo' => $formPhoto->createView(),
-            'animal' => $animal ?? null,
-            'edit_mode' => $editMode,
-        ]);
-    }*/
-
-    #[Route('/', name: 'index')]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(EntityManagerInterface $entityManager): Response
     {
         $animals = $entityManager->getRepository(Animal::class)->findAll();
